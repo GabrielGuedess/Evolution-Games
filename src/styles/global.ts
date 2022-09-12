d120117f1ab4f4abf1027051274c4fc482bdbdd0
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-12 05:29:20 -0300

    refactor: add new font and configuration in global styles
    add new font Inter regular and medium, remove images unused and configuration new font in the global
    styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,29 +1,21 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
     @font-face {
-      font-family: 'Poppins';
-      font-style: normal;
-      font-weight: 300;
-      font-display: swap;
-      src: local(''),
-          url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
-    }
-    @font-face {
-      font-family: 'Poppins';
+      font-family: 'Inter';
       font-style: normal;
       font-weight: 400;
       font-display: swap;
       src: local(''),
-          url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+          url('/fonts/inter-v12-latin-regular.woff2') format('woff2'),
     }
     @font-face {
-      font-family: 'Poppins';
+      font-family: 'Inter';
       font-style: normal;
-      font-weight: 600;
+      font-weight: 500;
       font-display: swap;
       src: local(''),
-          url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+          url('/fonts/inter-v12-latin-500.woff2') format('woff2'),
     }
 
   * {
@@ -32,19 +24,22 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    
-    
-    &::before,
-    &::after {
+
+    &:is(::before, ::after) {
       box-sizing: inherit;
     }
   }
+
   html {
     font-size: 62.5%;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    ${({ theme }) => css`
+      background: ${theme.colors.mainBg};
+      font-family: ${theme.font.family.primary};
+      color: ${theme.colors.whiteText};
+    `}
   }
 `;
 
