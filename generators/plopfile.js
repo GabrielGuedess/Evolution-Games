c38e25d6a098823cc4120c7ea7089a0bbd6db6b7
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-12 05:14:48 -0300

    chore: change handlebars files
    change handlebars files for design atomic in React

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,32 +1,37 @@
 module.exports = plop => {
   plop.setGenerator('component', {
-    description: 'Create a component',
+    description: 'criando uma estrutura para um componente',
     prompts: [
       {
         type: 'input',
+        name: 'type',
+        message: 'Qual o tipo do componente (atoms, molecules, organisms)?',
+      },
+      {
+        type: 'input',
         name: 'name',
-        message: 'What is your component name?',
+        message: 'Qual o nome do componente?',
       },
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/index.tsx',
-        templateFile: 'templates/Component.tsx.hbs',
+        path: '../src/components/{{type}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/index.tsx.hbs',
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/styles.ts',
-        templateFile: 'templates/styles.ts.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        path: '../src/components/{{type}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
         templateFile: 'templates/stories.tsx.hbs',
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
+        path: '../src/components/{{type}}/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{type}}/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
         templateFile: 'templates/test.tsx.hbs',
       },
     ],
