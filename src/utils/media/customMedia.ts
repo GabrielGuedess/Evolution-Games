fe6694e37c26989a7981b73798a1b73568cde9b3
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-12 05:47:52 -0300

    feat: create custom breaking points
    create custom breaking points with styled-media-query

---
+++ src/utils/media/customMedia.ts
@@ -0,0 +1,10 @@
+import { generateMedia } from 'styled-media-query';
+
+export const customMedia = generateMedia({
+  max: '1656px',
+  huge: '1440px',
+  large: '1170px',
+  big: '1024px',
+  medium: '768px',
+  small: '450px',
+});
