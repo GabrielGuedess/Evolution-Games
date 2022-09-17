560bfbff366438f9e6091082eec9935888fbeed4
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-16 23:47:23 -0300

    feat: add new component <MediaMatch /> in the project
    add new component MediaMatch based on design with styles, stories and test

---
+++ src/components/atoms/MediaMatch/MediaMatch.tsx
@@ -0,0 +1,28 @@
+import media, { DefaultBreakpoints } from 'styled-media-query';
+
+import styled, { css } from 'styled-components';
+
+type breakpoint = keyof DefaultBreakpoints;
+
+export type MediaMatchProps = {
+  lessThan?: breakpoint;
+  greaterThan?: breakpoint;
+};
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoint) => css`
+    ${media.lessThan(size)` display: block `}
+  `,
+
+  greaterThan: (size: breakpoint) => css`
+    ${media.greaterThan(size)` display: block `}
+  `,
+};
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`;
