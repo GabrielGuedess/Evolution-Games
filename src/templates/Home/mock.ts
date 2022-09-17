b4b6445a6de159e5a8682af933ba9af072f0b125
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-16 23:55:13 -0300

    feat: add mock in Home
    add mock file in the Home template

---
+++ src/templates/Home/mock.ts
@@ -0,0 +1,8 @@
+import { AvatarUserProps } from 'components/molecules/AvatarUser/AvatarUser';
+
+export const userInfo: AvatarUserProps = {
+  name: 'Gabriel Guedes',
+  username: 'gabrielrguedess',
+  bio: 'Fullstack Web Developer. Working with Javascript, Typescript, ReactJS, NextJS and NodeJS.',
+  userPhoto: 'https://avatars.githubusercontent.com/u/64827875?v=4',
+};
