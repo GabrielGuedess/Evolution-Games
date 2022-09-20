6caf62c71f7f820d5f0aa0d23907aaf5b84d7592
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-20 00:59:16 -0300

    fix: add meta robots in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="robots" content="noindex" />
         <meta name="theme-color" content="#04040C" />
         <meta name="description" content="A melhor loja de jogos do mundo" />
       </Head>
