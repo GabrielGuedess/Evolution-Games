d6c1b04d01cd6cbe5387789e693e5c15873ca769
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-09-20 01:25:16 -0300

    feat: add Slider component

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,4 +1,6 @@
 import { Navbar } from 'components/organisms/Navbar/Navbar';
+import { SliderMock } from 'components/organisms/Slider/mock';
+import { Slider } from 'components/organisms/Slider/Slider';
 
 import * as S from './Home.styles';
 import { userInfo } from './mock';
@@ -6,5 +8,6 @@ import { userInfo } from './mock';
 export const Home = () => (
   <S.Wrapper>
     <Navbar {...userInfo} />
+    <Slider slides={SliderMock} />
   </S.Wrapper>
 );
