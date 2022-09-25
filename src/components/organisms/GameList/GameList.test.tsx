d4ab815b04d1b70f009c6b549b3317c1cb315a33
Rafael Batista <rafaabatistas@gmail.com>
2022-09-25 14:59:22 -0300

    test: testing the <GameList /> component

--- src/components/organisms/GameList/GameList.test.tsx
+++ src/components/organisms/GameList/GameList.test.tsx
@@ -1,6 +1,8 @@
 import { renderWithTheme } from 'utils/tests/helpers';
 
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+
+import theme from 'styles/theme';
 
 import { GameList } from './GameList';
 import gameCardItems from './mock';
@@ -22,13 +24,14 @@ jest.mock('swiper/react', () => ({
       </div>
     );
   },
+
   SwiperSlide: function Mock({ children }: { children: React.ReactNode }) {
     return <div data-testid="Swiper-Slide">{children}</div>;
   },
 }));
 
 describe('<GameList />', () => {
-  it('should render the heading', () => {
+  it('should render the <GameList />', () => {
     const { container } = renderWithTheme(
       <GameList title="Bestsellers" data={gameCardItems} />,
     );
@@ -38,5 +41,61 @@ describe('<GameList />', () => {
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should filter by Xbox games', () => {
+    renderWithTheme(<GameList title="Bestsellers" data={gameCardItems} />);
+
+    // Arrange
+    const [icon] = screen.getAllByLabelText('Xbox Icon');
+
+    // Act
+    fireEvent.click(icon);
+
+    // Assert
+    expect(icon.parentElement).toHaveStyle({ color: theme.colors.primary });
+    expect(screen.getAllByTestId('Swiper-Slide')).toHaveLength(4);
+  });
+
+  it('should filter by PlayStation exclusive games', () => {
+    renderWithTheme(<GameList title="Bestsellers" data={gameCardItems} />);
+
+    // Arrange
+    const [icon] = screen.getAllByLabelText('PlayStation Icon');
+
+    // Act
+    fireEvent.click(icon);
+
+    // Assert
+    expect(icon.parentElement).toHaveStyle({ color: theme.colors.primary });
+    expect(screen.getAllByTestId('Swiper-Slide')).toHaveLength(9);
+  });
+
+  it('should filter by PC exclusive games', () => {
+    renderWithTheme(<GameList title="Bestsellers" data={gameCardItems} />);
+
+    // Arrange
+    const [icon] = screen.getAllByLabelText('PC Icon');
+
+    // Act
+    fireEvent.click(icon);
+
+    // Assert
+    expect(icon.parentElement).toHaveStyle({ color: theme.colors.primary });
+    expect(screen.getAllByTestId('Swiper-Slide')).toHaveLength(5);
+  });
+
+  it('should render all games', () => {
+    renderWithTheme(<GameList title="Bestsellers" data={gameCardItems} />);
+
+    // Arrange
+    const [icon] = screen.getAllByLabelText('All Platforms Icon');
+
+    // Act
+    fireEvent.click(icon);
+
+    // Assert
+    expect(icon.parentElement).toHaveStyle({ color: theme.colors.primary });
+    expect(screen.getAllByTestId('Swiper-Slide')).toHaveLength(9);
   });
 });
