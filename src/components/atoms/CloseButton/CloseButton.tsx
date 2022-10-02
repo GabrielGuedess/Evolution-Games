f82b91424d6215c4f4ce0863d4e874a40aa75dcb
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-10-01 23:23:58 -0300

    feat: add new props in <CloseButton />

--- src/components/atoms/CloseButton/CloseButton.tsx
+++ src/components/atoms/CloseButton/CloseButton.tsx
@@ -2,8 +2,18 @@ import { X } from 'phosphor-react';
 
 import * as S from './CloseButton.styles';
 
-export const CloseButton = () => (
-  <S.Close aria-label="Close Action">
+export type CloseButtonProps = {
+  side?: 'left' | 'right';
+  color?: 'primary' | 'white';
+  as?: React.ElementType;
+};
+
+export const CloseButton = ({
+  side = 'left',
+  color = 'primary',
+  ...props
+}: CloseButtonProps) => (
+  <S.Close side={side} color={color} {...props} aria-label="Close Action">
     <X size={20} />
   </S.Close>
 );
