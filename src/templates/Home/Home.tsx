88cc75245b027134e4d0597ae27e003545954be9
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-10-01 23:47:29 -0300

    feat: add <HighlightCarousel /> in <Home />

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,4 +1,6 @@
 import { GameList } from 'components/organisms/GameList/GameList';
+import { HighlightCarousel } from 'components/organisms/HighlightCarousel/HighlightCarousel';
+import { highlightMock } from 'components/organisms/HighlightCarousel/mock';
 import { Navbar } from 'components/organisms/Navbar/Navbar';
 import { Slider } from 'components/organisms/Slider/Slider';
 
@@ -9,7 +11,9 @@ export const Home = () => (
   <S.Wrapper>
     <Navbar {...userInfo} />
     <Slider slides={sliderMock} />
+
     <GameList title="Bestsellers" data={gameCardItems} />
+    <HighlightCarousel title="Pre-order" data={highlightMock} />
     <GameList title="Most Popular Games" data={gameCardItems} />
   </S.Wrapper>
 );
