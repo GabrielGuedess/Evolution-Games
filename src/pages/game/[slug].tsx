4a2392112dbd10d47c744076145983c4fb8bec2c
Rafael Batista <rafaabatistas@gmail.com>
2022-10-06 18:57:38 -0300

    feat: using getServerSideProps instead of getStaticProps

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 
 import { Game, GameProps } from 'templates/Game/Game';
@@ -10,24 +10,7 @@ export default function Index(props: GameProps) {
 
   return <Game {...props} />;
 }
-
-export async function getStaticPaths() {
-  return {
-    paths: [
-      { params: { slug: 'the-witcher-3-wild-hund' } },
-      { params: { slug: 'god-of-war-ragnarok' } },
-      { params: { slug: 'horizon-forbidden-west' } },
-      { params: { slug: 'elden-ring' } },
-      { params: { slug: 'star-wars-jedi-fallen-order' } },
-      { params: { slug: 'marvels-spider-man' } },
-      { params: { slug: 'the-last-of-us-part-ii' } },
-      { params: { slug: 'cyberpunk-2077' } },
-    ],
-    fallback: false,
-  };
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const host = process.env.HOST || 'http://localhost:3000';
   const data = await fetch(`${host}/api/games/${params?.slug}`);
   const game = await data.json();
