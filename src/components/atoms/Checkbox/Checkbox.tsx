30b3410fba1cd6afff9ec0bb47b605022a37528f
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-10-12 05:46:45 -0300

    refactor: change for controlled checkbox

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,46 +1,76 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
 
 import * as S from './Checkbox.styles';
 
 export type CheckboxProps = {
   title: string;
+  isChecked?: boolean;
   icon?: JSX.Element;
+  value?: string | ReadonlyArray<string> | number;
+  onCheck?: (status: boolean) => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const Checkbox = ({ title, icon, ...props }: CheckboxProps) => (
-  <S.Wrapper>
-    <S.CheckboxWrapper htmlFor={title}>
-      <S.Checkbox type="checkbox" id={title} {...props} />
+export const Checkbox = ({
+  title,
+  isChecked = false,
+  icon,
+  value,
+  onCheck,
+  ...props
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(isChecked);
 
-      <S.Icon viewBox="0 0 24 24">
-        <g
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-        >
-          <rect x="1" y="1" width="22" height="22" />
+  const onChange = () => {
+    const status = !checked;
+    setChecked(status);
 
-          <polyline
-            className="box-worm"
-            points="23,1 1,1 1,23 23,23 23,4"
-            strokeDasharray="30 146"
-            strokeDashoffset="30"
-          />
-          <polyline
-            className="check-worm"
-            points="23,4 10,17 5,12 18,12"
-            strokeDasharray="17.38 149.68"
-            strokeDashoffset="103.38"
-          />
-        </g>
-      </S.Icon>
+    if (!!onCheck) {
+      onCheck(status);
+    }
+  };
 
-      <S.WrapperInfos>
-        {icon}
+  return (
+    <S.Wrapper>
+      <S.CheckboxWrapper htmlFor={title}>
+        <S.Checkbox
+          onChange={onChange}
+          checked={checked}
+          value={value}
+          type="checkbox"
+          id={title}
+          {...props}
+        />
 
-        <S.Title>{title}</S.Title>
-      </S.WrapperInfos>
-    </S.CheckboxWrapper>
-  </S.Wrapper>
-);
+        <S.Icon viewBox="0 0 24 24">
+          <g
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          >
+            <rect x="1" y="1" width="22" height="22" />
+
+            <polyline
+              className="box-worm"
+              points="23,1 1,1 1,23 23,23 23,4"
+              strokeDasharray="30 146"
+              strokeDashoffset="30"
+            />
+            <polyline
+              className="check-worm"
+              points="23,4 10,17 5,12 18,12"
+              strokeDasharray="17.38 149.68"
+              strokeDashoffset="103.38"
+            />
+          </g>
+        </S.Icon>
+
+        <S.WrapperInfos>
+          {icon}
+
+          <S.Title>{title}</S.Title>
+        </S.WrapperInfos>
+      </S.CheckboxWrapper>
+    </S.Wrapper>
+  );
+};
