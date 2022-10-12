4020574bdbc3fd603566417d0e0f3b15869f9dc1
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-10-12 06:00:53 -0300

    feat: add new template component <Explorer /> in the project

---
+++ src/templates/Explorer/Explorer.test.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable import-helpers/order-imports */
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { filterItems } from 'components/organisms/ExplorerSidebar/mock';
+
+import { Explorer } from './Explorer';
+
+import { game } from './mock';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+useRouter.mockImplementation(() => ({
+  push: jest.fn(),
+  query: { genre: ['sports'] },
+}));
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useInfiniteQuery = jest.spyOn(require('react-query'), 'useInfiniteQuery');
+
+const fetchNextPage = jest.fn();
+
+useInfiniteQuery.mockImplementation(() => ({
+  data: {
+    pages: [[game]],
+  },
+  isFetching: false,
+  isLoading: false,
+  fetchNextPage,
+  hasNextPage: true,
+  getNextPageParam: () => ({}),
+}));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+jest.mock('templates/Base/Base', () => ({
+  __esModule: true,
+  Base: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Base Mock">{children}</div>;
+  },
+}));
+
+describe('<Explorer />', () => {
+  it('should render the components correctly', () => {
+    const { container } = renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByTestId('Base Mock')).toBeInTheDocument();
+    expect(screen.getByLabelText('ExplorerSidebar Mock')).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the RadioButton', async () => {
+    renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    await userEvent.click(screen.getByLabelText('Button Sort'));
+
+    expect(
+      screen.getByRole('radio', { name: 'Popularity' }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('radio', { name: 'High to low' }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('radio', { name: 'Low to high' }),
+    ).toBeInTheDocument();
+  });
+
+  it('should change checked RadioButtons', async () => {
+    renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    await userEvent.click(screen.getByLabelText('Button Sort'));
+
+    const radioPopularity = screen.getByRole('radio', { name: 'Popularity' });
+    const radioHighToLow = screen.getByRole('radio', { name: 'High to low' });
+    const radioLowToHigh = screen.getByRole('radio', { name: 'Low to high' });
+
+    expect(radioPopularity).toBeChecked();
+
+    await userEvent.click(radioHighToLow);
+
+    expect(radioHighToLow).toBeChecked();
+
+    await userEvent.click(radioLowToHigh);
+
+    expect(radioLowToHigh).toBeChecked();
+
+    await userEvent.click(radioPopularity);
+
+    expect(radioPopularity).toBeChecked();
+  });
+
+  it('should open Explorer Sidebar when click in button', async () => {
+    renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    await userEvent.click(screen.getAllByLabelText('Button Action')[0]);
+
+    expect(screen.getByLabelText('ExplorerSidebar Mock')).toHaveStyle({
+      visibility: 'visible',
+      'pointer-events': 'all',
+    });
+  });
+
+  it('should render more games', async () => {
+    renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    await userEvent.click(screen.getAllByLabelText('Button Action')[1]);
+
+    expect(fetchNextPage).toBeCalled();
+  });
+
+  it('should render query selected', () => {
+    renderWithTheme(
+      <QueryClientProvider client={queryClient}>
+        <Explorer filterItems={filterItems} />
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByLabelText('active-filter: sports')).toBeInTheDocument();
+  });
+});
---
+++ src/templates/Explorer/Explorer.tsx
@@ -0,0 +1,195 @@
+import { useRouter } from 'next/router';
+
+import { useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+import * as Popover from '@radix-ui/react-popover';
+import { CaretDown, FunnelSimple, List } from 'phosphor-react';
+import { ParsedUrlQueryInput } from 'querystring';
+import type Game from 'types/game';
+import { parseQueryStringToFilter } from 'utils/filter';
+
+import { Base } from 'templates/Base/Base';
+
+import Button from 'components/atoms/Button/Button';
+import { Empty } from 'components/atoms/Empty/Empty';
+import MediaMatch from 'components/atoms/MediaMatch/MediaMatch';
+import { Preloader } from 'components/atoms/Preloader/Preloader';
+import { RadioButton } from 'components/atoms/RadioButton/RadioButton';
+import { GameCard } from 'components/molecules/GameCard/GameCard';
+import {
+  ExplorerSidebar,
+  type ItemProps,
+} from 'components/organisms/ExplorerSidebar/ExplorerSidebar';
+
+import * as S from './Explorer.styles';
+
+export type ExplorerTemplateProps = {
+  filterItems: ItemProps[];
+};
+
+const fetchGames = async ({ pageParam = 1 }) =>
+  (
+    await fetch(
+      `${
+        process.env.VERCEL_URL || 'http://localhost:3000'
+      }/api/games?page=${pageParam}`,
+    )
+  ).json();
+
+export const Explorer = ({ filterItems }: ExplorerTemplateProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [secondaryFilter, setSecondaryFilter] = useState('Popularity');
+
+  const { push, query } = useRouter();
+
+  const { data, isFetching, isLoading, fetchNextPage, hasNextPage } =
+    useInfiniteQuery<Game[]>('games', fetchGames, {
+      getNextPageParam: (_, allPages) =>
+        allPages.length < 2 && allPages.length + 1,
+    });
+
+  const handleFilter = (items: ParsedUrlQueryInput) => {
+    push(
+      {
+        pathname: '/explorer',
+        query: items,
+      },
+      undefined,
+      { scroll: false },
+    );
+  };
+
+  const filterQuery = filterItems.map(item =>
+    item.fields.filter(field =>
+      Array.isArray(query[item.name])
+        ? query[item.name]?.includes(field.name)
+        : field.name === query[item.name],
+    ),
+  );
+
+  return (
+    <Base hasMarginTop hasColors>
+      <S.Wrapper>
+        <ExplorerSidebar
+          initialValues={parseQueryStringToFilter({
+            queryString: query,
+            filterItems,
+          })}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          items={filterItems}
+          onFilter={handleFilter}
+        />
+
+        <S.WrapperContent>
+          {data?.pages ? (
+            <>
+              <S.WrapperHeaderFilter>
+                <MediaMatch greaterThan="medium">
+                  <S.Filters>
+                    {filterQuery[0].map(fieldFilter => (
+                      <S.FilterTitle
+                        key={fieldFilter.name}
+                        aria-label={`active-filter: ${fieldFilter.name}`}
+                      >
+                        {fieldFilter.title}
+                      </S.FilterTitle>
+                    ))}
+                  </S.Filters>
+                </MediaMatch>
+
+                <MediaMatch lessThan="medium">
+                  <Button
+                    size="large"
+                    onClick={() => setIsOpen(!isOpen)}
+                    icon={<FunnelSimple size={20} />}
+                  >
+                    Filtrar
+                  </Button>
+                </MediaMatch>
+
+                <Popover.Root>
+                  <S.WrapperFilterSecondary aria-label="Button Sort">
+                    {secondaryFilter}
+                    <List size={20} />
+                  </S.WrapperFilterSecondary>
+                  <Popover.Portal>
+                    <S.ContentFilterSecondary align="center" sideOffset={8}>
+                      <RadioButton
+                        title="Popularity"
+                        value="popularity"
+                        name="filter-secondary"
+                        defaultChecked={secondaryFilter === 'Popularity'}
+                        onCheck={() => setSecondaryFilter('Popularity')}
+                      />
+                      <RadioButton
+                        title="High to low"
+                        value="high-to-low"
+                        name="filter-secondary"
+                        defaultChecked={secondaryFilter === 'High to low'}
+                        onCheck={() => setSecondaryFilter('High to low')}
+                      />
+                      <RadioButton
+                        title="Low to high"
+                        value="low-to-high"
+                        name="filter-secondary"
+                        defaultChecked={secondaryFilter === 'Low to high'}
+                        onCheck={() => setSecondaryFilter('Low to high')}
+                      />
+                      <S.Arrow />
+                    </S.ContentFilterSecondary>
+                  </Popover.Portal>
+                </Popover.Root>
+              </S.WrapperHeaderFilter>
+
+              <S.Grid>
+                {data?.pages.map(group =>
+                  group.map(game => (
+                    <GameCard
+                      key={game.name}
+                      id={game.id}
+                      name={game.name}
+                      slug={game.slug}
+                      genres={game.genres}
+                      developer={game.developer}
+                      releaseDate={game.releaseDate}
+                      image={game.image}
+                      score={game.score}
+                      price={game.price}
+                      platform={game.platform}
+                      primaryColor={game.primaryColor}
+                    />
+                  )),
+                )}
+              </S.Grid>
+
+              {hasNextPage && (
+                <S.More>
+                  {isFetching ? (
+                    <Preloader />
+                  ) : (
+                    <Button
+                      onClick={() => fetchNextPage()}
+                      minimal
+                      icon={<CaretDown size={20} />}
+                    >
+                      Mostrar Mais
+                    </Button>
+                  )}
+                </S.More>
+              )}
+            </>
+          ) : isLoading ? (
+            <Preloader />
+          ) : (
+            <Empty
+              title="Nenhum resultado encontrado"
+              description="Infelizmente não achamos nenhum resultado para a sua busca."
+            />
+          )}
+        </S.WrapperContent>
+      </S.Wrapper>
+    </Base>
+  );
+};
