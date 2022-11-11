369bbfdb83b83c0e416988a7374d07821c85e0a2
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-10 23:56:41 -0300

    feat: add providers in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -6,6 +8,7 @@ import { useState } from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 import { CartProvider } from 'hooks/useCart/useCart';
+import { FavoriteProvider } from 'hooks/useFavorite/useFavorite';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -19,33 +22,40 @@ import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 
-function App({ Component, pageProps }: AppProps<{ dehydratedState: unknown }>) {
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ dehydratedState: unknown; session: Session }>) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <CartProvider>
-          <ThemeProvider theme={theme}>
-            <Head>
-              <title>Evolution Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta name="robots" content="noindex" />
-              <meta name="theme-color" content="#04040C" />
-              <meta
-                name="description"
-                content="A melhor loja de jogos do mundo"
-              />
-            </Head>
-            <DefaultSeo {...SEO} />
-            <GlobalStyles />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </CartProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <FavoriteProvider>
+            <CartProvider>
+              <ThemeProvider theme={theme}>
+                <Head>
+                  <title>Evolution Games</title>
+                  <link rel="shortcut icon" href="/img/icon-512.png" />
+                  <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                  <link rel="manifest" href="/manifest.json" />
+                  <meta name="robots" content="noindex" />
+                  <meta name="theme-color" content="#04040C" />
+                  <meta
+                    name="description"
+                    content="A melhor loja de jogos do mundo"
+                  />
+                </Head>
+                <DefaultSeo {...SEO} />
+                <GlobalStyles />
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </CartProvider>
+          </FavoriteProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
 
