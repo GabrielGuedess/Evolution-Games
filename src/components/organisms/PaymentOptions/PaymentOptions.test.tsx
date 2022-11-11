8edc3194460abe1b1013240e0d19802f66931182
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-10 23:30:15 -0300

    feat: add new component <PaymentOptions />

---
+++ src/components/organisms/PaymentOptions/PaymentOptions.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import { PaymentOptions } from './PaymentOptions';
+
+describe('<PaymentOptions />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<PaymentOptions />);
+
+    expect(
+      screen.getByRole('heading', { name: /PaymentOptions/i }),
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
