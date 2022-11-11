6ffda3f5d13431a77ce65e948de18a2d668b9695
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-10 23:12:43 -0300

    refactor: change type for api return

--- src/types/game.ts
+++ src/types/game.ts
@@ -2,8 +2,7 @@ import { SlideImage } from 'yet-another-react-lightbox';
 
 import { Platform } from 'components/atoms/PlatformIcon/PlatformIcon';
 
-export type PCSystemItem = {
-  type: 'minimal' | 'recommended';
+export type PCSystemDefault = {
   so: string;
   cpu: string;
   memory: string;
@@ -11,23 +10,38 @@ export type PCSystemItem = {
   hd: string;
 };
 
-type Game = {
-  id?: string;
+export type PCSystem = {
+  minimal: PCSystemDefault;
+  recommended: PCSystemDefault;
+};
+
+export type Default = {
+  id: string;
   name: string;
   slug: string;
-  genres: string[];
-  developer: string;
-  releaseDate: Date | number | string;
+};
+
+export type PlatformData = {
+  slug: Platform;
+} & Omit<Default, 'slug'>;
+
+type Game = {
+  id: string;
+  name: string;
+  slug: string;
+  release_date: Date | number | string;
   score: number;
-  price: number;
-  platform: Platform;
-  image: string;
-  background: string;
   video?: string;
+  image: string;
+  primary_color: string;
+  background: string;
   description: string;
-  gallery: SlideImage[];
-  primaryColor: string;
-  pcSystem?: [PCSystemItem, PCSystemItem];
+  price: number;
+  genres: Default[];
+  developers: Default[];
+  platforms: PlatformData[];
+  games_gallery: SlideImage[];
+  pc_system: PCSystem | null;
 };
 
 export default Game;
